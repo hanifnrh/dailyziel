1ac777ec1bf30eab40d07113b19df206b0f350e6
hanirnrh <nurrahmathaniff@gmail.com>
2025-02-05 16:04:58 +0700

    Selection

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
                 allow: ["/"],
             },
         ],
-        sitemap: "https://dailyziel.com/sitemap.xml",
+        sitemap: "https://www.dailyziel.com/sitemap.xml",
     };
 }
